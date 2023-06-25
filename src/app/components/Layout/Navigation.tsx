1bf9b81ebcc9e4292dbccbf071f48c91b4625d81
frozenal <jkchisolm22@gmail.com>
2023-06-24 20:11:11 -0700

    project init

---
+++ src/app/components/Layout/Navigation.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+type Props = {};
+
+const Navigation = (props: Props) => {
+  return (
+    <div className="bg-zinc-900 my-2 ml-2 mr-1 rounded py-4">Navigation</div>
+  );
+};
+
+export default Navigation;
---
+++ src/lib/fonts/GothamFont.ts
@@ -0,0 +1,48 @@
+import localFont from "@next/font/local";
+
+const gotham = localFont({
+  src: [
+    {
+      path: "../../assets/fonts/GothamBook.ttf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../assets/fonts/GothamBold.ttf",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../../assets/fonts/GothamMedium.ttf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../../assets/fonts/GothamLight.ttf",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../../assets/fonts/GothamBookItalic.ttf",
+      weight: "400",
+      style: "italic",
+    },
+    {
+      path: "../../assets/fonts/GothamBoldItalic.ttf",
+      weight: "700",
+      style: "italic",
+    },
+    {
+      path: "../../assets/fonts/GothamMediumItalic.ttf",
+      weight: "500",
+      style: "italic",
+    },
+    {
+      path: "../../assets/fonts/GothamLightItalic.ttf",
+      weight: "300",
+      style: "italic",
+    },
+  ],
+});
+
+export default gotham;
